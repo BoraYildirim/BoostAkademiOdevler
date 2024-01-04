1def56cc13610eb206eba5441816d1d49f5c9d0e
BoraYildirim <155664768+BoraYildirim@users.noreply.github.com>
2024-01-04 20:14:23 +0300

    First Homework Done

---
+++ borayildirimOdev/borayildirimOdev/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("ilk sayiyi giriniz");
+double sayi1 = double.Parse(Console.ReadLine());
+Console.WriteLine("ikinci sayiyi giriniz");
+double sayi2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Yapmak istediğiniz işlemi giriniz");
+string islem = Console.ReadLine();
+double toplam = 0;
+if (islem == "toplama")
+{ toplam = sayi1 + sayi2;
+    Console.WriteLine(toplam); }
+else if (islem == "çarpma")
+{ toplam = sayi1 * sayi2;
+    Console.WriteLine(toplam); }
+else if (islem == "çıkarma")
+{
+        toplam = sayi1 - sayi2;
+        Console.WriteLine(toplam);
+}
+else if (islem == "bölme")
+{ toplam = sayi1 / sayi2;
+    Console.WriteLine(toplam); }
+else
+{ Console.WriteLine("Geçersiz işlem tekrar deneyiniz"); }
